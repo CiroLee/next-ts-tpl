884554f6c772d64c60fa0d2cf0548901cfec1e48
CiroLee <ciromail@126.com>
2025-09-06 12:43:56 +0800

    feat: add max-len lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,22 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      'max-len': [
+        'error',
+        {
+          code: 200,
+          ignorePattern: '^(\\s*<path\\s+d="[^"]+"|\\s*<svg\\s+[^>]+>)', // 同时忽略svg和path的长属性
+          ignoreUrls: true,
+          ignoreStrings: true, // 忽略长字符串
+          ignoreTemplateLiterals: true, // 忽略模板字符串
+          ignoreRegExpLiterals: true, // 忽略正则表达式
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
